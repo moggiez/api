a97574e1a7aae1a88bc69976e7f538cd0886d49d
GeorgiStavrev <georgi@insurify.com>
2021-07-03 20:01:33 +0300

    scripts cleanup and update package.json of the db lib

--- scripts/build_and_package_layer.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-echo "Packaging $1 to $2/$3"
-cd $1 && zip -r $2/$3 ./
--- scripts/package_all.sh
+++ scripts/package_all.sh
@@ -4,18 +4,10 @@ CODE_DIR=$PWD/code
 LIBS_DIR=$PWD/code/libs
 DIST_DIR=$PWD/dist
 APIS=("user" "organisation" "domain" "playbook" "loadtest" "metrics")
-LAYERS=("db" "auth" "lambda_helpers" "metrics")
  
 for api in "${APIS[@]}"
 do
     echo "Building api '$api'..."
 	$PWD/scripts/build_and_package_api.sh $CODE_DIR/$api $DIST_DIR ${api}_api.zip
-    echo ""
-done
-
-for layer in "${LAYERS[@]}"
-do
-    echo "Building layer '$layer'..."
-	$PWD/scripts/build_and_package_layer.sh $LIBS_DIR/$layer $DIST_DIR ${layer}_layer.zip
     echo ""
 done
