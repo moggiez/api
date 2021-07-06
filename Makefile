99be297b988bd58135cdf945ff943398247fc006
GeorgiStavrev <georgi@insurify.com>
2021-07-06 16:05:23 +0300

    fremoves metrics_api module, sets up handler, adds lambda publishing via aws cli

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+# CODE COMMANDS
 version-build:
 	./increment_version.sh
 
@@ -19,25 +20,31 @@ format:
 test:
 	jest
 
+update-lambda-fn:
+	aws lambda update-function-code --function-name ${FUNC_NAME} --zip-file fileb://$(shell pwd)/dist/${FUNC_NAME}.zip --publish | jq .FunctionArn
+
+# INFRASTRUCTURE COMMANDS
 infra-init:
 	cd infrastructure && terraform init -force-copy -backend-config="bucket=moggies.io-terraform-state-backend" -backend-config="dynamodb_table=moggies.io-api-terraform_state" -backend-config="key=api-terraform.state" -backend-config="region=eu-west-1"
 
 infra-debug:
 	cd infrastructure && TF_LOG=DEBUG terraform apply -auto-approve infra
 
-deploy: build modules-cleanup
+infra-deploy: build modules-cleanup
 	cd infrastructure && terraform init && terraform apply -auto-approve
 
-preview: build modules-cleanup
+infra-preview: build modules-cleanup
 	cd infrastructure && terraform init && terraform plan
 
-fmt:
+infra-fmt:
 	cd infrastructure && terraform fmt -recursive
 
 undeploy:
 	cd infrastructure && terraform destroy
 
+# NPM COMMANDS
 repo-publish:
 	./scripts/publish_npm_packages.sh
+
 npm-auth:
 	aws codeartifact login --tool npm --repository team-npm --domain moggies-io --domain-owner 989665778089
--- code/metrics/index.js
+++ code/metrics/index.js
@@ -1,31 +1,35 @@
 "use strict";
 
-const auth = require("moggies-auth");
-const config = require("./config");
-const handlers = require("./handlers");
+const AWS = require("aws-sdk");
+const db = require("moggies-db");
 const helpers = require("moggies-lambda-helpers");
+const auth = require("moggies-auth");
+const metricsHelpers = require("moggies-metrics");
 
-exports.handler = async function (event, context, callback) {
+const config = require("./config");
+const { Handler } = require("./handler");
+
+exports.handler = function (event, context, callback) {
   const response = helpers.getResponseFn(callback);
-  const request = helpers.getRequestFromEvent(event);
-  const user = auth.getUserFromEvent(event);
 
   if (config.DEBUG) {
-    response(200, event, config.headers);
+    response(200, event);
   }
 
-  try {
-    const loadtestId = request.getPathParamAtIndex(0, "");
-    const metricName = request.getPathParamAtIndex(1, "ResponseTime");
-    if (request.httpMethod == "GET") {
-      await handlers.get(user, loadtestId, metricName, response);
-    } else if (request.httpMethod == "POST") {
-      await handlers.post(user, loadtestId, metricName, request.body, response);
-    } else {
-      response(500, "Not supported.", config.headers);
-    }
-  } catch (err) {
-    console.log(err);
-    response(500, err, config.headers);
-  }
+  const user = auth.getUserFromEvent(event);
+  const request = helpers.getRequestFromEvent(event);
+  request.user = user;
+
+  const organisations = new db.Table(db.tableConfigs.organisations);
+  const loadtests = new db.Table(db.tableConfigs.loadtests);
+  const loadtest_metrics = new db.Table(db.tableConfigs.loadtest_metrics);
+  const CloudWatch = new AWS.CloudWatch({ apiVersion: "2010-08-01" });
+  const Metrics = new metricsHelpers.Metrics(CloudWatch);
+  const handler = new Handler(
+    organisations,
+    loadtests,
+    loadtest_metrics,
+    Metrics
+  );
+  handler.handle(request, response);
 };
