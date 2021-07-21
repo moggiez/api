a54bc0f9a9d0953a66fb90f7157bc981aaae08f9
GeorgiStavrev <georgi@insurify.com>
2021-07-21 18:12:20 +0300

    removes metrics api

--- infrastructure/api_domain_mapping.tf
+++
@@ -1,9 +0,0 @@
-module "playbook_api_subdomain_mapping" {
-  source          = "git@github.com:moggiez/terraform-modules.git//api_subdomain_mapping"
-  api             = aws_api_gateway_rest_api._
-  api_stage_name  = local.stage
-  api_subdomain   = "api"
-  certificate_arn = aws_acm_certificate._.arn
-  domain_name     = "moggies.io"
-  hosted_zone_id  = local.hosted_zone.zone_id
-}
--- scripts/package_all.sh
+++ scripts/package_all.sh
@@ -9,16 +9,7 @@ LOCAL_ENV=prod
    LOCAL_ENV=dev ||
    LOCAL_ENV=prod
 
-APIS=("metrics")
 PACKAGES=("db" "auth" "lambda_helpers" "metrics")
-
-for api in "${APIS[@]}"
-do
-    echo "Building api '$api'..."
-    rm -r $CODE_DIR/$api/node_modules
-	ENV=$LOCAL_ENV $PWD/scripts/build_and_package_api.sh $CODE_DIR/$api $DIST_DIR ${api}_api.zip
-    echo ""
-done
 
 for pkg in "${PACKAGES[@]}"
 do
